754ef89d9a48dacfd6aec95d8caf2a3db8966d76
Mudassir Ahmed <mudassirahmedmuhammadtariq@gmail.com>
2026-07-10 23:15:07 +0500

    Minor Commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,12 @@ import Paste from './components/Paste'
 function App() {
   const router = createBrowserRouter([
     {
+      // Home Path
       path: '/',
       element: <div><Navbar /><Home /></div>
     },
     {
+      // Paste Path
       path: '/paste',
       element: <div><Navbar /><Paste /></div>
     }
